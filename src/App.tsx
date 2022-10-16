b9c127c4dcb11339a03008a7f5726cd4be2ced74
Lautaro Andreani <lautaro.andreani@gmail.com>
2022-10-16 17:21:32 -0300

    feat: icons & pizza list cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import Home from "./Views/Home"
 import Lists from "./Views/Lists"
 
 function App() {
-	return (
-		<div>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/lists" element={<Lists/>} />
-			</Routes>
-		</div>
-	)
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lists" element={<Lists />} />
+        <Route path="/lists/:local" element={<Lists />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
